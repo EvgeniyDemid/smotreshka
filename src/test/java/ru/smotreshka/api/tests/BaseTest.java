493710efce58933a602dee9e2aa074055ab5358c
Demidov, Yevgeny <Yevgeny.Demidov@nedra.digital>
2023-08-30 23:37:36 +0300

    System.getProperty

--- src/test/java/ru/smotreshka/api/tests/BaseTest.java
+++ src/test/java/ru/smotreshka/api/tests/BaseTest.java
@@ -1,7 +1,6 @@
 package ru.smotreshka.api.tests;
 
 import com.codeborne.selenide.Configuration;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -13,14 +12,9 @@ public class BaseTest {
 	static WebConfig config = ConfigFactory.create(WebConfig.class, System.getProperties());
 	String email = config.login();
 	String password = config.password();
+
 	@BeforeAll
 	static void beforeAll() {
-		Configuration.baseUrl = System.getProperty("baseUrl", config.baseUrl());
-		Configuration.pageLoadStrategy = "eager";
-		Configuration.browserSize = System.getProperty("browserSize", config.browserSize());
-		Configuration.browser = System.getProperty("browser", config.browser());
-		Configuration.remote = System.getProperty("selenoid");
-		Configuration.browserVersion = System.getProperty("browserVersion", config.browserVersion());
 
 		DesiredCapabilities capabilities = new DesiredCapabilities();
 		capabilities.setCapability("selenoid:options", Map.<String, Object>of(
